d4dd709859f185e85185b8a48c32f1331a01fb08
Ola Połaniecka <97olka@gmail.com>
2024-03-27 17:53:51 +0100

    update

--- server.js
+++ server.js
@@ -1,7 +1,26 @@
 const app = require("./app");
+const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log("Server running. Use our API on port: 3000");
 });
+
+const MONGODB_URI =
+  "mongodb+srv://97olka:<password>@cluster0.q4ue9io.mongodb.net/";
+
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: false,
+};
+
+mongoose
+  .connect(MONGODB_URI, options)
+  .then(() => {
+    console.log("Database connection successful");
+  })
+  .catch((error) => {
+    console.error("Database connection error:", error);
+    process.exit(1);
+  });
